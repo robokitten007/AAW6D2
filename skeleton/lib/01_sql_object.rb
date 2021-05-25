4d7d4e8886cdb29f0534b8b32911ff31c270d5bf
wliu <wliu0709@gmail.com>
2021-05-25 18:03:25 -0400

    attr_accessor and sql_object

--- skeleton/lib/01_sql_object.rb
+++ skeleton/lib/01_sql_object.rb
@@ -5,22 +5,40 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL)
+    SELECT *
+    FROM #{self.table_name}
+    SQL
+    @columns = cols.first.map{|name| name.to_sym}
+   
   end
 
-  def self.finalize!
+  def self.finalize!  #class method
+    self.columns.each do |col|
+      define_method(col) do 
+        self.attributes[col]
+      end 
+
+      define_method("#{col}=") do |value|
+        self.attributes[col] = value
+      end 
+    end 
+
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
+    @table_name || self.name.underscore.pluralize
+
     # ...
   end
 
   def self.all
-    # ...
+    
   end
 
   def self.parse_all(results)
@@ -31,12 +49,19 @@ class SQLObject
     # ...
   end
 
-  def initialize(params = {})
-    # ...
+  def initialize(params = {}) #instance method
+      params.each do |attr_name, value|
+        attr_name = attr_name.to_sym
+        if self.class.columns.include?(attr_name)  #self.class to access class method within an instance
+          self.send("#{attr_name}=", value)
+        else 
+          raise "unknown attribute '#{attr_name}'"
+        end 
+      end 
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
